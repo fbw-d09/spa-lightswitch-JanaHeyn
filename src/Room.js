bba352bbadbd1ab6737f62ae9a9c5057493397fa
JanaHeyn <jana.heyn@web.de>
2023-06-15 15:36:31 +0200

    Update room className

--- src/Room.js
+++ src/Room.js
@@ -6,11 +6,11 @@ export const Room = () => {
     const [ lightState, setLightState ] = useState(true);
 
     const handleLightState = () => {
-        setLightState(!lightState)
+        setLightState(!lightState);
     };
 
     return(
-        <div className={ lightState ? 'lit' : 'dark' }>
+        <div className={ `${lightState ? 'lit' : 'dark'} room` } >
             <p>The room is { `${lightState ? 'lit' : 'dark'}` }</p>
             <button onClick={ handleLightState }>Lightswitch</button>
         </div>
